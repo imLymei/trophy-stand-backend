48acbe98f96b4c9dcad91a3c64b33b9a399940f8
imlymei <lymeicontato@gmail.com>
2026-09-09 22:47:23 -0300

    feat: add basic flask api

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deal Hunter API
+
+WIP
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from deal-hunter-api!")
+from flask import redirect
+from flask_cors import CORS
+from flask_openapi3.models.info import Info
+from flask_openapi3.openapi import OpenAPI
 
 
-if __name__ == "__main__":
-    main()
+def create_app(database_uri: str | None = None) -> OpenAPI:
+    app = OpenAPI(__name__, info=Info(title="DealHunter API", version="0.1.0"))
+
+    if database_uri is not None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///vault.db"
+
+    CORS(app)
+
+    @app.route("/")
+    def root() -> str:
+        return "Hello World"
+
+    @app.route("/docs")
+    def docs_redirect():
+        return redirect("/openapi/swagger", code=302)
+
+    return app
